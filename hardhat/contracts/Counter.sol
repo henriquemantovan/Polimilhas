fcc979885bdb480868e6db607bbece1d03775054
Kiyochi <shojidan@gmail.com>
2024-12-05 21:04:08 -0300

    hardhat

--- hardhat/contracts/Counter.sol
+++ hardhat/contracts/Counter.sol
@@ -1,18 +1,50 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+// SPDX-License-Identifier: GPL-3.0
 
-contract Counter {
-    int public i;
 
-    constructor(int _i) {
-        i = _i;
+pragma solidity >=0.8.2 <0.9.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract PoliMilhas is ERC20("PoliMilhas", "PM"){
+    address public owner;
+    
+    event TokenRedeemed(address user, uint256 amount);  
+    event TokensSentToContract(address sender, uint256 amount);
+
+    constructor() {
+        owner = msg.sender; 
+    }
+    function CreateNewToken(uint256 amount) public{
+        require(msg.sender == owner);
+        _mint(address(this), amount);
+
+    }
+    
+    function sendToContract(uint qnt) public{
+        require(msg.sender.balance >= qnt, "Not enough Tokens");
+        _transfer(address(msg.sender), address(this), qnt);
+          emit TokensSentToContract(msg.sender, qnt);
+    
+    }
+    function sendTokens (address recipient, uint256 amount) public {
+        require(msg.sender == owner, "Only the owner can send tokens from the contract");
+        require(balanceOf(address(msg.sender)) >= amount, "Contract does not have enough tokens");
+        _transfer(address(msg.sender), recipient, amount);
     }
 
-    function inc() public {
-        i++;
+    //100 Gwei = 1 PM
+    function buyTokens() public payable {
+        require(msg.value >= 100 gwei, "Minimum purchase is 100 Gwei");
+        uint256 tokenAmount = msg.value / 100 gwei;
+        require(totalSupply() >= tokenAmount, "Contract does not have enough tokens");
+         _transfer(address(this), msg.sender, tokenAmount);
     }
 
-    function dec() public {
-        i--;
-    }
-}
+   function RedeemToken(uint price) public payable{
+        require(msg.sender.balance >= price, "Not enough Tokens");
+        _transfer(msg.sender, address(this), price);
+        emit TokenRedeemed(msg.sender, price);
+   }
+
+
+
+}   
--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -3,13 +3,15 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 import dotenv from "dotenv";
 
 dotenv.config();
-const SEPOLIA_PRIVATE_KEY = process.env["SEPOLIA_PRIVATE_KEY"] ?? "";
-const ALCHEMY_API_KEY = process.env["ALCHEMY_API_KEY"] ?? "";
-const ETHERSCAN_API_KEY = process.env["ETHERSCAN_API_KEY"] ?? "";
+
+
+// const SEPOLIA_PRIVATE_KEY = process.env["SEPOLIA_PRIVATE_KEY"] ?? "";
+// const ALCHEMY_API_KEY = process.env["ALCHEMY_API_KEY"] ?? "";
+// const ETHERSCAN_API_KEY = process.env["ETHERSCAN_API_KEY"] ?? "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
-  networks: {
+  /*networks: {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY],
@@ -17,7 +19,8 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
-  },
+  },*/
 };
 
 export default config;
+  
