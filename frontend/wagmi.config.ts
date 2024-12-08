fd95b36b955aa2ed202e8a6fa897dc46759d29aa
GusTenshou <gustavo.x.amaral7@gmail.com>
2024-12-07 23:38:35 -0300

    wagmi setup

--- frontend/wagmi.config.ts
+++ frontend/wagmi.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "@wagmi/cli";
 import { hardhat } from "@wagmi/cli/plugins";
 import { react } from "@wagmi/cli/plugins";
+import { abi } from "../hardhat/artifacts/contracts/PoliMilhas.sol/PoliMilhas.json";
 
 export default defineConfig({
   out: "src/generated.ts",
